886ac734374a4b3fd02a37a5c21f69d2c6221e97
Firch <47253246+Firch@users.noreply.github.com>
2024-03-17 17:22:29 +0500

    Joker descriptions rework

--- Bunco.lua
+++ Bunco.lua
@@ -13,7 +13,7 @@ function SMODS.INIT.Bunco()
         local bunco_mod = SMODS.findModByID("Bunco")
 
     -- Dread joker:
-        SMODS.Sprite:new("j_dread", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register();
+        SMODS.Sprite:new("j_dread", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register()
 
         local loc_dread = {
             ["name"] = "Dread",
@@ -48,7 +48,7 @@ function SMODS.INIT.Bunco()
         end
     
     -- Cassette joker:
-    SMODS.Sprite:new("j_cassette", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register();
+    SMODS.Sprite:new("j_cassette", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register()
 
     local loc_cassette = {
         ["name"] = "Cassette",
@@ -83,7 +83,7 @@ function SMODS.INIT.Bunco()
     end
 
     -- Mosaic joker:
-    SMODS.Sprite:new("j_mosaic", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register();
+    SMODS.Sprite:new("j_mosaic", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register()
 
     local loc_mosaic = {
         ["name"] = "Mosaic Joker",
@@ -117,138 +117,137 @@ function SMODS.INIT.Bunco()
         end
     end
 
-     -- Voxel joker:
-     SMODS.Sprite:new("j_voxel", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register();
+    -- Voxel joker:
+    SMODS.Sprite:new("j_voxel", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register()
 
-     local loc_voxel = {
-         ["name"] = "Voxel Joker",
-         ["text"] = {
-             [1] = "AHAHA",
-             [2] = "Jokes on",
-             [3] = "{C:attention}You{}!"
-         }
-     }
+    local loc_voxel = {
+        ["name"] = "Voxel Joker",
+        ["text"] = {
+            [1] = "AHAHA",
+            [2] = "Jokes on",
+            [3] = "{C:attention}You{}!"
+        }
+    }
  
-     -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
-     local joker_voxel = SMODS.Joker:new(
-         "Voxel Joker", -- Name
-         "voxel", -- Slug
-         {}, -- Config
-         {x = 3, y = 0}, -- Sprite position
-         loc_voxel, -- Localization
-         1, 4) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
+    -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
+    local joker_voxel = SMODS.Joker:new(
+        "Voxel Joker", -- Name
+        "voxel", -- Slug
+        {}, -- Config
+        {x = 3, y = 0}, -- Sprite position
+        loc_voxel, -- Localization
+        1, 4) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
  
-     joker_voxel:register()
+    joker_voxel:register()
  
-     SMODS.Jokers.j_voxel.calculate = function(self, context)
-         if SMODS.end_calculate_context(context) then
-             return {
-                 mult_mod = 20,
-                 card = self,
-                 colour = G.C.RED,
-                 message = "AHAHAHAH"
-             }
+    SMODS.Jokers.j_voxel.calculate = function(self, context)
+        if SMODS.end_calculate_context(context) then
+            return {
+                mult_mod = 20,
+                card = self,
+                colour = G.C.RED,
+                message = "AHAHAHAH"
+            }
  
-         end
-     end
+        end
+    end
 
-     -- Crop circles joker:
-     SMODS.Sprite:new("j_crop", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register();
+    -- Crop circles joker:
+    SMODS.Sprite:new("j_crop", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register()
 
-     local loc_crop = {
-         ["name"] = "Crop Circles",
-         ["text"] = {
-             [1] = "AHAHA",
-             [2] = "Jokes on",
-             [3] = "{C:attention}You{}!"
-         }
-     }
+    local loc_crop = {
+        ["name"] = "Crop Circles",
+        ["text"] = {
+            [1] = "AHAHA",
+            [2] = "Jokes on",
+            [3] = "{C:attention}You{}!"
+        }
+    }
  
-     -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
-     local joker_crop = SMODS.Joker:new(
-         "Crop Circles", -- Name
-         "crop", -- Slug
-         {}, -- Config
-         {x = 4, y = 0}, -- Sprite position
-         loc_crop, -- Localization
-         1, 4) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
+    -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
+    local joker_crop = SMODS.Joker:new(
+        "Crop Circles", -- Name
+        "crop", -- Slug
+        {}, -- Config
+        {x = 4, y = 0}, -- Sprite position
+        loc_crop, -- Localization
+        1, 4) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
  
-     joker_crop:register()
+    joker_crop:register()
  
-     SMODS.Jokers.j_crop.calculate = function(self, context)
-         if SMODS.end_calculate_context(context) then
-             return {
-                 mult_mod = 20,
-                 card = self,
-                 colour = G.C.RED,
-                 message = "AHAHAHAH"
-             }
+    SMODS.Jokers.j_crop.calculate = function(self, context)
+        if SMODS.end_calculate_context(context) then
+            return {
+                mult_mod = 20,
+                card = self,
+                colour = G.C.RED,
+                message = "AHAHAHAH"
+            }
  
-         end
-     end
+        end
+    end
 
-     -- X-Ray joker:
-     SMODS.Sprite:new("j_xray", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register();
+    -- X-Ray joker:
+    SMODS.Sprite:new("j_xray", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register()
 
-     local loc_xray = {
-         ["name"] = "X-Ray",
-         ["text"] = {
-             [1] = "AHAHA",
-             [2] = "Jokes on",
-             [3] = "{C:attention}You{}!"
-         }
-     }
+    local loc_xray = {
+        ["name"] = "X-Ray",
+        ["text"] = {
+            [1] = "AHAHA",
+            [2] = "Jokes on",
+            [3] = "{C:attention}You{}!"
+        }
+    }
  
-     -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
-     local joker_xray = SMODS.Joker:new(
-         "X-Ray", -- Name
-         "xray", -- Slug
-         {}, -- Config
-         {x = 5, y = 0}, -- Sprite position
-         loc_xray, -- Localization
-         2, 4) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
+    -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
+    local joker_xray = SMODS.Joker:new(
+        "X-Ray", -- Name
+        "xray", -- Slug
+        {}, -- Config
+        {x = 5, y = 0}, -- Sprite position
+        loc_xray, -- Localization
+        1, 4) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
  
-     joker_xray:register()
+    joker_xray:register()
  
-     SMODS.Jokers.j_xray.calculate = function(self, context)
-         if SMODS.end_calculate_context(context) then
-             return {
-                 mult_mod = 20,
-                 card = self,
-                 colour = G.C.RED,
-                 message = "AHAHAHAH"
-             }
+    SMODS.Jokers.j_xray.calculate = function(self, context)
+        if SMODS.end_calculate_context(context) then
+            return {
+                mult_mod = 20,
+                card = self,
+                colour = G.C.RED,
+                message = "AHAHAHAH"
+            }
  
-         end
-     end
+        end
+    end
 
-     -- Prehistoric joker:
-     SMODS.Sprite:new("j_oonga", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register();
+    -- Prehistoric joker:
+    SMODS.Sprite:new("j_oonga", bunco_mod.path, "Jokers.png", 71, 95, "asset_atli"):register()
 
-     local loc_oonga = {
-         ["name"] = "Prehistoric Joker",
-         ["text"] = {
-             [1] = "{C:red}+16{} Mult for each",
-             [2] = "card of the same rank and suit",
-             [3] = "that was already",
-             [4] = "played during this round"
-         }
-     }
+    local loc_oonga = {
+        ["name"] = "Prehistoric Joker",
+        ["text"] = {
+            [1] = "{C:mult}+#1#{} Mult for each",
+            [2] = "card of the same rank and suit",
+            [3] = "that was already",
+            [4] = "played during this round"
+        }
+    }
  
-     -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
-     local joker_oonga = SMODS.Joker:new(
-         "Prehistoric Joker", -- Name
-         "oonga", -- Slug
-         {mult = 16, extra = {card_list = { }}}, -- Config
-         {x = 0, y = 1}, -- Sprite position
-         loc_oonga, -- Localization
-         2, 4) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
+    -- SMODS.Joker:new(name, slug, config, spritePos, loc_txt, rarity, cost, unlocked, discovered, blueprint_compat, eternal_compat)
+    local joker_oonga = SMODS.Joker:new(
+        "Prehistoric Joker", -- Name
+        "oonga", -- Slug
+        {mult = 16, extra = {card_list = { }}}, -- Config
+        {x = 0, y = 1}, -- Sprite position
+        loc_oonga, -- Localization
+        2, 6) -- Rarity & Cost. 1 - Common, 2 - Uncommon, 3 - Rare, 4 - Legendary
  
-     joker_oonga:register()
- 
-     SMODS.Jokers.j_oonga.calculate = function(self, context)
-        self.ability.mult = 16 -- This is a stinky fix to mult being equal to ten for some reason
+    joker_oonga:register()
 
+    SMODS.Jokers.j_oonga.calculate = function(self, context)
+        
         if context.individual and context.cardarea == G.play then
 
             for k, v in pairs(self.ability.extra.card_list) do
@@ -257,7 +256,7 @@ function SMODS.INIT.Bunco()
                         message = localize {
                             type = 'variable',
                             key = 'a_mult',
-                            vars = { self.ability.mult }
+                            vars = {self.ability.mult}
                         },
                         mult = self.ability.mult,
                         card = self
@@ -267,12 +266,48 @@ function SMODS.INIT.Bunco()
 
             table.insert(self.ability.extra.card_list, context.other_card.base.id .. context.other_card.base.suit) -- Add card to the list
 
-            end
-
-            if context.end_of_round then -- Clear the list if end of round
-                self.ability.extra.card_list = {};
-
         end
+
+        if context.end_of_round then -- Clear the list if end of round
+            self.ability.extra.card_list = {}
+        end
+    end
+
+    -- Jokers' descriptions:
+    local card_uiref = Card.generate_UIBox_ability_table
+
+ 
+    function Card:generate_UIBox_ability_table()
+
+        if self.ability.name == "Dread" then
+            return generate_card_ui(self.config.center, nil, nil, self.ability.set, {}, false, nil, nil)
+        end
+
+        if self.ability.name == "Cassette" then
+            return generate_card_ui(self.config.center, nil, nil, self.ability.set, {}, false, nil, nil)
+        end
+
+        if self.ability.name == "Mosaic Joker" then
+            return generate_card_ui(self.config.center, nil, nil, self.ability.set, {}, false, nil, nil)
+        end
+
+        if self.ability.name == "Voxel Joker" then
+            return generate_card_ui(self.config.center, nil, nil, self.ability.set, {}, false, nil, nil)
+        end
+
+        if self.ability.name == "Crop Circles" then
+            return generate_card_ui(self.config.center, nil, nil, self.ability.set, {}, false, nil, nil)
+        end
+
+        if self.ability.name == "X-Ray" then
+            return generate_card_ui(self.config.center, nil, nil, self.ability.set, {}, false, nil, nil)
+        end
+
+        if self.ability.name == "Prehistoric Joker" then
+            return generate_card_ui(self.config.center, nil, {self.ability.mult}, self.ability.set, {}, false, nil, nil)
+        end
+        
+        return card_uiref(self)
     end
 end
 
