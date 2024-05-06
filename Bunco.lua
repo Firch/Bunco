0d98c51d9b0d8bfff9b5afabe5a83f4ed2338c9f
Firch <47253246+Firch@users.noreply.github.com>
2024-05-07 02:20:32 +0500

    Minor changes, stinkiest code for blind's variables

--- Bunco.lua
+++ Bunco.lua
@@ -1250,7 +1250,7 @@ function SMODS.INIT.Bunco()
          -- Doorhanger Joker additional function (\DOOR_FUN)
          -- I do not create card immideately for this one because of the sound Jokers make when they have edition
 
-         if G.jokers ~= nil then
+        if G.jokers ~= nil then
             for _, v in ipairs(G.jokers.cards) do
                 if v.ability.name == 'Doorhanger' and not v.debuff then
                     if _rarity == nil or _rarity < 0.9 then
@@ -1859,7 +1859,7 @@ function SMODS.INIT.Bunco()
 
     SMODS.Jokers.j_dread.calculate = function(self, context)
 
-        if context.full_hand ~= nil then
+        if context.full_hand ~= nil and context.full_hand then
             self.ability.extra.trash_list = {}
             for k, v in ipairs(context.full_hand) do
                 table.insert(self.ability.extra.trash_list, v)
@@ -1878,7 +1878,6 @@ function SMODS.INIT.Bunco()
 
             G.E_MANAGER:add_event(Event({
                 trigger = 'after',
-                delay = 0,
                 func = function()
 
                     for i = 1, #self.ability.extra.trash_list do
@@ -3478,20 +3477,22 @@ function SMODS.INIT.Bunco()
     function end_round()
         original_end_round()
 
-        if G.GAME.blind:get_type() == 'Boss' then
-            local _handname, _played, _order = 'High Card', math.huge, 100
-            for k, v in pairs(G.GAME.hands) do
-                if v.played < _played or (v.played == _played and v.order < _order) then
-                    _played = v.played
-                    _handname = k
-                    _order = v.order
-                    break
-                end
-            end
-            G.GAME.current_round.least_played_poker_hand = _handname
+        local lowestValue = math.huge
+        local leastPlayedHand = ''
 
-            sendDebugMessage(G.GAME.current_round.least_played_poker_hand)
+        for i = #G.handlist, 1, -1 do
+            local v = G.handlist[i]
+            local playedCount = G.GAME.hands[v].played
+            if playedCount < lowestValue then
+                lowestValue = playedCount
+                leastPlayedHand = v
+            end
         end
+
+        G.GAME.current_round.least_played_poker_hand = leastPlayedHand
+
+        sendDebugMessage(G.GAME.current_round.least_played_poker_hand)
+
     end
 
     -- Blind Initializing (\BL_INI)
@@ -3569,7 +3570,7 @@ function SMODS.INIT.Bunco()
         'The Mask', -- Name
         'mask', -- Slug
         {name = 'The Mask',
-        text = {'#2# has the base', 'Chips and Mult of #1#'}},
+        text = {'#1# has the base', 'Chips and Mult of #2#'}},
         5, -- Reward
         2, -- Multiplier
         {}, -- Vars
@@ -3633,7 +3634,8 @@ function SMODS.INIT.Bunco()
             if not self.disabled then
                 local loc_vars = nil
                 if self.name == 'The Mask' then
-                    loc_vars = {localize(G.GAME.current_round.most_played_poker_hand, 'poker_hands'),
+                    loc_vars = {
+                    localize(G.GAME.current_round.most_played_poker_hand, 'poker_hands'),
                     localize(G.GAME.current_round.least_played_poker_hand, 'poker_hands')}
 
                     local loc_target = localize{type = 'raw_descriptions', key = self.config.blind.key, set = 'Blind', vars = loc_vars or self.config.blind.vars}
@@ -3655,6 +3657,225 @@ function SMODS.INIT.Bunco()
         end
     end
 
+    function create_UIBox_blind_choice(type, run_info) -- Very stinky! No lovely makes people go insane
+        if not G.GAME.blind_on_deck then
+            G.GAME.blind_on_deck = 'Small'
+        end
+        if not run_info then G.GAME.round_resets.blind_states[G.GAME.blind_on_deck] = 'Select' end
+        local disabled = false
+        type = type or 'Small'
+        local blind_choice = {
+            config = G.P_BLINDS[G.GAME.round_resets.blind_choices[type]],
+        }
+        local blind_atlas = 'blind_chips'
+        if blind_choice.config and blind_choice.config.atlas then
+            blind_atlas = blind_choice.config.atlas
+        end
+        blind_choice.animation = AnimatedSprite(0, 0, 1.4, 1.4, G.ANIMATION_ATLAS[blind_atlas], blind_choice.config.pos)
+        blind_choice.animation:define_draw_steps({
+            { shader = 'dissolve', shadow_height = 0.05 },
+            { shader = 'dissolve' }
+        })
+        local extras = nil
+        local stake_sprite = get_stake_sprite(G.GAME.stake or 1, 0.5)
+        G.GAME.orbital_choices = G.GAME.orbital_choices or {}
+        G.GAME.orbital_choices[G.GAME.round_resets.ante] = G.GAME.orbital_choices[G.GAME.round_resets.ante] or {}
+        if not G.GAME.orbital_choices[G.GAME.round_resets.ante][type] then
+            local _poker_hands = {}
+            for k, v in pairs(G.GAME.hands) do
+                if v.visible then _poker_hands[#_poker_hands + 1] = k end
+            end
+            G.GAME.orbital_choices[G.GAME.round_resets.ante][type] = pseudorandom_element(_poker_hands, pseudoseed('orbital'))
+        end
+        if type == 'Small' then
+            extras = create_UIBox_blind_tag(type, run_info)
+        elseif type == 'Big' then
+            extras = create_UIBox_blind_tag(type, run_info)
+        elseif not run_info then
+            local dt1 = DynaText({ string = { { string = localize('ph_up_ante_1'), colour = G.C.FILTER } }, colours = { G.C.BLACK }, scale = 0.55, silent = true, pop_delay = 4.5, shadow = true, bump = true, maxw = 3 })
+            local dt2 = DynaText({ string = { { string = localize('ph_up_ante_2'), colour = G.C.WHITE } }, colours = { G.C.CHANCE }, scale = 0.35, silent = true, pop_delay = 4.5, shadow = true, maxw = 3 })
+            local dt3 = DynaText({ string = { { string = localize('ph_up_ante_3'), colour = G.C.WHITE } }, colours = { G.C.CHANCE }, scale = 0.35, silent = true, pop_delay = 4.5, shadow = true, maxw = 3 })
+            extras =
+            {
+                n = G.UIT.R,
+                config = { align = "cm" },
+                nodes = {
+                    {
+                        n = G.UIT.R,
+                        config = { align = "cm", padding = 0.07, r = 0.1, colour = { 0, 0, 0, 0.12 }, minw = 2.9 },
+                        nodes = {
+                            {
+                                n = G.UIT.R,
+                                config = { align = "cm" },
+                                nodes = {
+                                    { n = G.UIT.O, config = { object = dt1 } },
+                                }
+                            },
+                            {
+                                n = G.UIT.R,
+                                config = { align = "cm" },
+                                nodes = {
+                                    { n = G.UIT.O, config = { object = dt2 } },
+                                }
+                            },
+                            {
+                                n = G.UIT.R,
+                                config = { align = "cm" },
+                                nodes = {
+                                    { n = G.UIT.O, config = { object = dt3 } },
+                                }
+                            },
+                        }
+                    },
+                }
+            }
+        end
+        G.GAME.round_resets.blind_ante = G.GAME.round_resets.blind_ante or G.GAME.round_resets.ante
+        local loc_target = localize{ type = 'raw_descriptions', key = blind_choice.config.key, set = 'Blind', vars = { localize(G.GAME.current_round.most_played_poker_hand, 'poker_hands'), localize(G.GAME.current_round.least_played_poker_hand, 'poker_hands') } }
+        local loc_name = localize{ type = 'name_text', key = blind_choice.config.key, set = 'Blind' }
+        local text_table = loc_target
+        local blind_col = get_blind_main_colour(type)
+        local blind_amt = get_blind_amount(G.GAME.round_resets.blind_ante) * blind_choice.config.mult *
+            G.GAME.starting_params.ante_scaling
+        local blind_state = G.GAME.round_resets.blind_states[type]
+        local _reward = true
+        if G.GAME.modifiers.no_blind_reward and G.GAME.modifiers.no_blind_reward[type] then _reward = nil end
+        if blind_state == 'Select' then blind_state = 'Current' end
+        local run_info_colour = run_info and
+            (blind_state == 'Defeated' and G.C.GREY or blind_state == 'Skipped' and G.C.BLUE or blind_state == 'Upcoming' and G.C.ORANGE or blind_state == 'Current' and G.C.RED or G.C.GOLD)
+        local t =
+        {
+            n = G.UIT.R,
+            config = { id = type, align = "tm", func = 'blind_choice_handler', minh = not run_info and 10 or nil, ref_table = { deck = nil, run_info = run_info }, r = 0.1, padding = 0.05 },
+            nodes = {
+                {
+                    n = G.UIT.R,
+                    config = { align = "cm", colour = mix_colours(G.C.BLACK, G.C.L_BLACK, 0.5), r = 0.1, outline = 1, outline_colour = G.C.L_BLACK },
+                    nodes = {
+                        {
+                            n = G.UIT.R,
+                            config = { align = "cm", padding = 0.2 },
+                            nodes = {
+                                not run_info and
+                                {
+                                    n = G.UIT.R,
+                                    config = { id = 'select_blind_button', align = "cm", ref_table = blind_choice.config, colour = disabled and G.C.UI.BACKGROUND_INACTIVE or G.C.ORANGE, minh = 0.6, minw = 2.7, padding = 0.07, r = 0.1, shadow = true, hover = true, one_press = true, button = 'select_blind' },
+                                    nodes = {
+                                        { n = G.UIT.T, config = { ref_table = G.GAME.round_resets.loc_blind_states, ref_value = type, scale = 0.45, colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.UI.TEXT_LIGHT, shadow = not disabled } }
+                                    }
+                                } or
+                                {
+                                    n = G.UIT.R,
+                                    config = { id = 'select_blind_button', align = "cm", ref_table = blind_choice.config, colour = run_info_colour, minh = 0.6, minw = 2.7, padding = 0.07, r = 0.1, emboss = 0.08 },
+                                    nodes = {
+                                        { n = G.UIT.T, config = { text = localize(blind_state, 'blind_states'), scale = 0.45, colour = G.C.UI.TEXT_LIGHT, shadow = true } }
+                                    }
+                                }
+                            }
+                        },
+                        {
+                            n = G.UIT.R,
+                            config = { id = 'blind_name', align = "cm", padding = 0.07 },
+                            nodes = {
+                                {
+                                    n = G.UIT.R,
+                                    config = { align = "cm", r = 0.1, outline = 1, outline_colour = blind_col, colour = darken(blind_col, 0.3), minw = 2.9, emboss = 0.1, padding = 0.07, line_emboss = 1 },
+                                    nodes = {
+                                        { n = G.UIT.O, config = { object = DynaText({ string = loc_name, colours = { disabled and G.C.UI.TEXT_INACTIVE or G.C.WHITE }, shadow = not disabled, float = not disabled, y_offset = -4, scale = 0.45, maxw = 2.8 }) } },
+                                    }
+                                },
+                            }
+                        },
+                        {
+                            n = G.UIT.R,
+                            config = { align = "cm", padding = 0.05 },
+                            nodes = {
+                                {
+                                    n = G.UIT.R,
+                                    config = { id = 'blind_desc', align = "cm", padding = 0.05 },
+                                    nodes = {
+                                        {
+                                            n = G.UIT.R,
+                                            config = { align = "cm" },
+                                            nodes = {
+                                                {
+                                                    n = G.UIT.R,
+                                                    config = { align = "cm", minh = 1.5 },
+                                                    nodes = {
+                                                        { n = G.UIT.O, config = { object = blind_choice.animation } },
+                                                    }
+                                                },
+                                                text_table[1] and
+                                                {
+                                                    n = G.UIT.R,
+                                                    config = { align = "cm", minh = 0.7, padding = 0.05, minw = 2.9 },
+                                                    nodes = {
+                                                        text_table[1] and {
+                                                            n = G.UIT.R,
+                                                            config = { align = "cm", maxw = 2.8 },
+                                                            nodes = {
+                                                                { n = G.UIT.T, config = { id = blind_choice.config.key, ref_table = { val = '' }, ref_value = 'val', scale = 0.32, colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.WHITE, shadow = not disabled, func = 'HUD_blind_debuff_prefix' } },
+                                                                { n = G.UIT.T, config = { text = text_table[1] or '-', scale = 0.32, colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.WHITE, shadow = not disabled } }
+                                                            }
+                                                        } or nil,
+                                                        text_table[2] and {
+                                                            n = G.UIT.R,
+                                                            config = { align = "cm", maxw = 2.8 },
+                                                            nodes = {
+                                                                { n = G.UIT.T, config = { text = text_table[2] or '-', scale = 0.32, colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.WHITE, shadow = not disabled } }
+                                                            }
+                                                        } or nil,
+                                                    }
+                                                } or nil,
+                                            }
+                                        },
+                                        {
+                                            n = G.UIT.R,
+                                            config = { align = "cm", r = 0.1, padding = 0.05, minw = 3.1, colour = G.C.BLACK, emboss = 0.05 },
+                                            nodes = {
+                                                {
+                                                    n = G.UIT.R,
+                                                    config = { align = "cm", maxw = 3 },
+                                                    nodes = {
+                                                        { n = G.UIT.T, config = { text = localize('ph_blind_score_at_least'), scale = 0.3, colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.WHITE, shadow = not disabled } }
+                                                    }
+                                                },
+                                                {
+                                                    n = G.UIT.R,
+                                                    config = { align = "cm", minh = 0.6 },
+                                                    nodes = {
+                                                        { n = G.UIT.O, config = { w = 0.5, h = 0.5, colour = G.C.BLUE, object = stake_sprite, hover = true, can_collide = false } },
+                                                        { n = G.UIT.B, config = { h = 0.1, w = 0.1 } },
+                                                        { n = G.UIT.T, config = { text = number_format(blind_amt), scale = score_number_scale(0.9, blind_amt), colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.RED, shadow = not disabled } }
+                                                    }
+                                                },
+                                                _reward and {
+                                                    n = G.UIT.R,
+                                                    config = { align = "cm" },
+                                                    nodes = {
+                                                        { n = G.UIT.T, config = { text = localize('ph_blind_reward'), scale = 0.35, colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.WHITE, shadow = not disabled } },
+                                                        { n = G.UIT.T, config = { text = string.rep(localize("$"), blind_choice.config.dollars) .. '+', scale = 0.35, colour = disabled and G.C.UI.TEXT_INACTIVE or G.C.MONEY, shadow = not disabled } }
+                                                    }
+                                                } or nil,
+                                            }
+                                        },
+                                    }
+                                },
+                            }
+                        },
+                    }
+                },
+                {
+                    n = G.UIT.R,
+                    config = { id = 'blind_extras', align = "cm" },
+                    nodes = {
+                        extras,
+                    }
+                }
+            }
+        }
+        return t
+    end
 end
 
 local generate_card_ui_ref = generate_card_ui
