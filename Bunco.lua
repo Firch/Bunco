ef26cb5e1f4672d7c7afa629926189ff2a15c248
Firch <47253246+Firch@users.noreply.github.com>
2024-12-21 20:47:04 +0500

    Boss Tag safeguard

--- Bunco.lua
+++ Bunco.lua
@@ -365,19 +365,21 @@ if config.gameplay_reworks then
         end,
         config = {type = 'new_blind_choice'},
         apply = function(self, tag, context)
-            tag:yep('+', G.C.SECONDARY_SET.Spectral, function()
-                local key = 'p_bunc_blind_'..(math.random(4))
-                local card = Card(G.play.T.x + G.play.T.w/2 - G.CARD_W*1.27/2,
-                G.play.T.y + G.play.T.h/2-G.CARD_H*1.27/2, G.CARD_W*1.27, G.CARD_H*1.27, G.P_CARDS.empty, G.P_CENTERS[key], {bypass_discovery_center = true, bypass_discovery_ui = true})
-                card.cost = 0
-                card.from_tag = true
-                G.FUNCS.use_card({config = {ref_table = card}})
-                card:start_materialize()
-                G.CONTROLLER.locks[tag.ID] = nil
+            if context.type == self.config.type then
+                tag:yep('+', G.C.SECONDARY_SET.Spectral, function()
+                    local key = 'p_bunc_blind_'..(math.random(4))
+                    local card = Card(G.play.T.x + G.play.T.w/2 - G.CARD_W*1.27/2,
+                    G.play.T.y + G.play.T.h/2-G.CARD_H*1.27/2, G.CARD_W*1.27, G.CARD_H*1.27, G.P_CARDS.empty, G.P_CENTERS[key], {bypass_discovery_center = true, bypass_discovery_ui = true})
+                    card.cost = 0
+                    card.from_tag = true
+                    G.FUNCS.use_card({config = {ref_table = card}})
+                    card:start_materialize()
+                    G.CONTROLLER.locks[tag.ID] = nil
+                    return true
+                end)
+                tag.triggered = true
                 return true
-            end)
-            tag.triggered = true
-            return true
+            end
         end
     })
 end
