45ee0ff2a3ee78ac855c7de7c80ef9817fb860eb
Firch <47253246+Firch@users.noreply.github.com>
2025-02-02 14:58:46 +0500

    Jumper nerf

--- Bunco.lua
+++ Bunco.lua
@@ -3513,7 +3513,7 @@ create_joker({ -- Games Collector
 
 create_joker({ -- Jumper
     name = 'Jumper', position = 59,
-    vars = {{bonus = 10}, {chips = 0}},
+    vars = {{bonus = 8}, {chips = 0}},
     rarity = 'Common', cost = 5,
     blueprint = true, eternal = true, perishable = false,
     unlocked = true,
