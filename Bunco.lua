89da06b4e0da12f7a3dee2edbdfba0a13ce44358
Firch <47253246+Firch@users.noreply.github.com>
2025-03-09 14:08:46 +0500

    Rasta nerf

--- Bunco.lua
+++ Bunco.lua
@@ -2996,7 +2996,7 @@ create_joker({ -- On Broadway
 
 create_joker({ -- Rasta
     name = 'Rasta', position = 48,
-    vars = {{mult = 20}},
+    vars = {{mult = 12}},
     rarity = 'Common', cost = 5,
     blueprint = true, eternal = true,
     unlocked = false,
